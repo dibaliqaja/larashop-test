6fca1cf25e354777d477c45c840b8b9aaa516b75
Iqbal <dibaliqaja@gmail.com>
2020-09-12 07:26:06 +0700

    #1 - First Commit

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\User;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Administrator',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('11111111'),
+            'role' => 'ADMIN',
+            'email_verified_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        User::create([
+            'name' => 'Adi Gumilang',
+            'email' => 'adi@gmail.com',
+            'password' => bcrypt('11111111'),
+            'role' => 'CUSTOMER',
+            'email_verified_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+    }
+}
